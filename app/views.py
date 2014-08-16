4d880da77c4a877c6378cb16bb4f10b4f3e14018
Sehmon Burnam <sehmon3@gmail.com>
2014-08-16 13:35:29 -0400

    Added comments in views.py

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from app import app, db
 from models import Printer
 import pdb
 
+# 'index' gives you the 5 schools and links to the printers in each
 @app.route('/')
 @app.route('/index')
 def index():
@@ -10,13 +11,13 @@ def index():
     schools = ['mhs', 'ait', 'apa', 'allied', 'uctech']
     return render_template('school.html', schools = schools)
 
-@app.route('/<school>')
+# 'school' gives you the rooms found in the schools
+@app.route('/<school>/')
 def school(school):
     printers = Printer.query.filter(Printer.building == school)
     return render_template('room.html', printers = printers)
 
-@app.route('/<school>/<room>')
-def room(school, room):
-    pdb.set_trace()
-    printers = printers.query.filter(Printer.room == room)
+@app.route('/<school>/<int:room>')
+def room(room):
+    printers2 = printers.query.filter(Printer.room == room)
     return render_template('printers.html', printers = printers) 
