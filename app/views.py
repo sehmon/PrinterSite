253766d4bcb220ca4101b0c24ee310d84beeb300
Sehmon Burnam <sehmon3@gmail.com>
2014-08-17 21:26:28 -0400

    Added Instruction landing page

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
-from flask import render_template
+from flask import render_template, g
 from app import app, db
 from models import Printer
 import pdb
 
-# 'index' gives you the 5 schools and links to the printers in each
+# 'index' returns the 5 schools and links to the printers in each
 @app.route('/')
 @app.route('/index')
 def index():
@@ -11,13 +11,20 @@ def index():
     schools = ['mhs', 'ait', 'apa', 'allied', 'uctech']
     return render_template('school.html', schools = schools)
 
-# 'school' gives you the rooms found in the schools
+# 'school' returns the rooms found in the schools
 @app.route('/<school>/')
 def school(school):
     printers = Printer.query.filter(Printer.building == school)
     return render_template('room.html', printers = printers)
 
-@app.route('/<school>/<int:room>')
+# 'room' returns the printers found in the specified room
+@app.route('/room/<int:room>')
 def room(room):
-    printers2 = printers.query.filter(Printer.room == room)
-    return render_template('printers.html', printers = printers) 
+    printers = Printer.query.filter(Printer.room == room)
+    return render_template('printers.html', printers = printers)
+
+# 'printer will return the instructions for a single printer'
+@app.route('/instructions/<int:id>')
+def printer(id):
+    printer = Printer.query.get(id)
+    return render_template('instructions.html', printer=printer)
